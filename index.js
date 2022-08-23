1cbeeb8da9c4085f9754f3c79e777dcbee2d697d
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-23 22:34:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+// let employee={
+//     name:,
+//     street:
+// }
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    let employspread={...employee,...key,...value};
+    console.log(employspread)
+}
+updateEmployeeWithKeyAndValue(employeeobj={
+    name:"mike",
+    key:"254",
+    value:"alot"
+},'location',"worth")
